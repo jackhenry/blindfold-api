1e1058eae5e91bf8be50db76100f7ccf27daf275
Jack Erickson <jackhenry.tech@gmail.com>
2019-11-12 19:13:30 -0600

    Create scoreboard schema

---
+++ src/scoreboard/schemas/scoreboard.schema.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose';
+
+export const ScoreBoardSchema = new mongoose.Schema({
+  homeTeamName: String,
+  homeTeamQuarters: [String],
+  homeTeamScore: String,
+  homeTeamRecord: String,
+  homeTeamImage: String,
+  homeTeamAbbreviation: String,
+  homeTeamColor: String,
+  awayTeamName: String,
+  awayTeamQuarters: [String],
+  awayTeamScore: String,
+  awayTeamRecord: String,
+  awayTeamImage: String,
+  awayTeamAbbreviation: String,
+  awayTeamColor: String,
+  date: Date,
+  league: String,
+  status: String,
+  week: Number,
+});
