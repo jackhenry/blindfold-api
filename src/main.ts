164468622450f54530b966121a70d4c59fa6bd6a
Jack Erickson <jackhenry.tech@gmail.com>
2019-11-15 21:36:25 -0500

    Added session and cors middleware.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cors from 'cors';
+import * as session from 'express-session';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cors());
+  //TODO: change secret
+  app.use(
+    session({
+      secret: 'test-secret',
+    }),
+  );
   await app.listen(3001);
 }
 bootstrap();
